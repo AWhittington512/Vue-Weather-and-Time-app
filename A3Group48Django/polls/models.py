3e4392635e210789fc4ea62665d1659e7da31e0c
Robertson Hollingsworth <rhollingsworth@torontomu.ca>
2023-04-04 13:23:28 -0400

    Added 'facilitator' attribute

--- A3Group48Django/polls/models.py
+++ A3Group48Django/polls/models.py
@@ -7,10 +7,13 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    facilitator = models.CharField(max_length=200, null=True)
     def __str__(self) :
         return self.question_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def facilitator_name(self) :
+        return self.facilitator
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
